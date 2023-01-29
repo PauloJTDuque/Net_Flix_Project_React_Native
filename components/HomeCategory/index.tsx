065f1b99339c01b23a24879117452461c6489194
Paulo Duque <paulo.duque@terra.com.br>
2023-01-28 21:29:18 -0300

    Net_Flix 280123

--- components/HomeCategory/index.tsx
+++ components/HomeCategory/index.tsx
@@ -6,24 +6,32 @@ import { Text, } from '../../components/Themed';
 
 import styles from './styles';
 
+interface HomeCategoryProps {
+  category: {
 
-const firstCategory = categories.items[0];
+    id: string,
+    title: string,
+    movies: {
+      id: string,
+      poster: string,
+    }[]
 
-const HomeCategory = () => {
+  }
+}
 
+const HomeCategory = (props: HomeCategoryProps) => {
+  const { category } = props;
   return (
-    <>
-        <Text style={styles.title}>Popular on Netflix</Text>
-       
-
-        <FlatList
-            data={firstCategory.movies} 
-            renderItem={({item}) => (
-                <Image style={styles.image} source={{ uri: item.poster}}/>
-            )}
-            horizontal
-        />
-    </>
+      <>
+            <Text style={styles.title}>{category.title}</Text>
+          <FlatList
+              data={category.movies} 
+              renderItem={({item}) => (
+                  <Image style={styles.image} source={{ uri: item.poster}}/>
+              )}
+              horizontal
+          />
+      </>
   );
 }
 
--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -7,14 +7,17 @@ import styles from './styles';
 import categories from '../../assets/data/categories';
 import HomeCategory from '../../components/HomeCategory';
 
-const firstCategory = categories.items[0];
+const firstCategory = categories.items[2];
 
 const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
         {/* List of Categories */}
-        <HomeCategory category={firstCategory}/>
+<FlatList
+    data= {categories.items}
+    renderItem={({item}) => <HomeCategory category={item} />}
+/>
 
     </View>
   );
