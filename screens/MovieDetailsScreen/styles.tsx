e2f06331114674c0cb1eb52899abdb5517c598a2
Paulo Duque <paulo.duque@terra.com.br>
2023-02-02 10:02:36 -0300

    Net_Flix 020223

--- screens/MovieDetailsScreen/styles.tsx
+++ screens/MovieDetailsScreen/styles.tsx
@@ -6,17 +6,58 @@ const styles = StyleSheet.create({
         aspectRatio: 16/9,
         resizeMode: 'cover',
     },
-    match:{
-
+    title:{
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    match: {
+        color: '#59d467',
+        fontWeight: 'bold',
+        marginRight: 5,
     },
     year:{
-
+        color: '#757575',
+        marginRight: 5,
     },
-    agecontainer:{
-
+    ageContainer:{
+        backgroundColor: '#d9d75a',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+        paddingHorizontal: 3,
+        marginRight: 5,
     },
     age:{
+        color: 'black',
+        fontWeight: 'bold',
+    },
 
+    // Buttons
+    playButton:{
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 5,
+        borderRadius: 3,
+        marginVertical: 5,
+    },
+    playButtonText:{
+        color: 'black',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    downloadButton:{
+        backgroundColor: '#2b2b2b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 5,
+        borderRadius: 3,
+        marginVertical: 5,
+    },
+    downloadButtonText:{
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
     },
 
 })
