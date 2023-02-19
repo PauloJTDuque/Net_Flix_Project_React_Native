a6c554db2efa008b55956f758bbfaf84329c2dc0
Paulo Duque <paulo.duque@terra.com.br>
2023-02-19 16:37:57 -0300

    Net_Flix 190223

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "netflix97e3ac0f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
